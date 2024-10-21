fc2d990baf2ed22968f57f0b9aa4c19a8676cdfb
lc3267 <lc3267@kit.edu>
2024-10-21 10:15:47 +0000

    rm output plotting
    Signed-off-by: lc3267 <lc3267@kit.edu>

--- src/fromhopetoheuristics/pipelines/visualization/nodes.py
+++ src/fromhopetoheuristics/pipelines/visualization/nodes.py
@@ -1,10 +1,7 @@
-import os
-
 from qallse.cli.func import (
     diff_rows,
     process_response,
 )
-from qallse.data_wrapper import DataWrapper
 from qallse.plotting import iplot_results
 
 import logging
@@ -12,18 +9,15 @@ import logging
 log = logging.getLogger(__name__)
 
 
-def visualize(responses, data_wrapper, event_path):
-
+def visualize(responses, data_wrapper):
     dims = list("xy")
-    figs = []
+    figs = {}
     for i, response in enumerate(responses):
-        output_path = os.path.join(event_path, "plots")
-        prefix = f"plot{i:02d}"
-        final_doublets, final_tracks = process_response(response)
+        final_doublets, _ = process_response(response)
         _, missings, _ = diff_rows(final_doublets, data_wrapper.get_real_doublets())
-        dout = os.path.join(output_path, prefix + "plot-doublets.html")
-        tout = os.path.join(output_path, prefix + "plot-triplets.html")
-        iplot_results(data_wrapper, final_doublets, missings, dims=dims, filename=dout)
-        figs.append((dout, tout))
+
+        figs[i] = iplot_results(
+            data_wrapper, final_doublets, missings, dims=dims, return_fig=True
+        )
 
     return {"figures": figs}
