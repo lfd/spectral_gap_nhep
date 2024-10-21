7d407853f16ba83457fa3c8be695babaf072538b
lc3267 <lc3267@kit.edu>
2024-10-21 14:34:24 +0000

    started converting schedules
    Signed-off-by: lc3267 <lc3267@kit.edu>

--- src/fromhopetoheuristics/pipelines/anneal_schedule/pipeline.py
+++ src/fromhopetoheuristics/pipelines/anneal_schedule/pipeline.py
@@ -1,21 +1,26 @@
 from kedro.pipeline import Pipeline, node, pipeline
 
-from .nodes import (
-    create_anneal_schedule,
-)
+from .nodes import create_maxcut_anneal_schedule, create_trackrecon_anneal_schedule
 
 
 def create_pipeline() -> Pipeline:
     return pipeline(
         [
             node(
-                create_anneal_schedule,
+                create_maxcut_anneal_schedule,
                 {
-                    "qaoa_result_file": "params:qaoa_result_file",
+                    "q": "params:q",
+                    "max_p": "params:max_p",
+                    "maxcut_max_qubits": "params:maxcut_max_qubits",
+                },
+                {},
+            ),
+            node(
+                create_trackrecon_anneal_schedule,
+                {
                     "q": "params:q",
                     "max_p": "params:max_p",
                     "num_angle_parts": "params:num_angle_parts",
-                    "maxcut_max_qubits": "params:maxcut_max_qubits",
                 },
                 {},
             ),
