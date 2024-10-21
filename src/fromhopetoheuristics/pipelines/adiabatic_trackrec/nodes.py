8987e2dc5e399930a12dae5312b853bd6e8dc9f8
lc3267 <lc3267@kit.edu>
2024-10-21 09:42:46 +0000

    direct result export
    Signed-off-by: lc3267 <lc3267@kit.edu>

--- src/fromhopetoheuristics/pipelines/adiabatic_trackrec/nodes.py
+++ src/fromhopetoheuristics/pipelines/adiabatic_trackrec/nodes.py
@@ -1,12 +1,10 @@
-import os
-from datetime import datetime
 import numpy as np
-from typing import Iterable, Optional, Dict
+from typing import Iterable, Dict
 
 from fromhopetoheuristics.utils.spectral_gap_calculator import (
     calculate_spectral_gap,
 )
-from fromhopetoheuristics.utils.data_utils import save_to_csv
+import pandas as pd
 import logging
 
 log = logging.getLogger(__name__)
@@ -14,67 +12,40 @@ log = logging.getLogger(__name__)
 
 def track_reconstruction_annealing(
     qubo: np.ndarray,
-    seed: int,
     fractions: Iterable[float],
-    geometric_index: int = 0,
-    include_header: bool = True,
 ):
-    csv_data_list = []
-    if include_header:
-        csv_data_list.append(
-            [
-                "problem",
-                "num_qubits",
-                "geometric_index",
-                "seed",
-                "fraction",
-                "gs",
-                "fes",
-                "gap",
-            ]
-        )
+    gs_energies = []
+    fes_energies = []
+    gaps = []
 
     for fraction in fractions:
         gs_energy, fes_energy, gap = calculate_spectral_gap(
             fraction,
             qubo,
         )
-        csv_data_list.append(
-            [
-                "track reconstruction",
-                len(qubo),
-                geometric_index,
-                seed,
-                np.round(fraction, 2),
-                gs_energy,
-                fes_energy,
-                gap,
-            ]
-        )
+        gs_energies.append(gs_energy)
+        fes_energies.append(fes_energy)
+        gaps.append(gap)
 
-    # for csv_data in csv_data_list:
-    # save_to_csv(csv_data, result_path_prefix, "spectral_gap_evolution.csv")
-    return {"results": csv_data_list}
+    return {"gs_energies": gs_energies, "fes_energies": fes_energies, "gaps": gaps}
 
 
 def run_track_reconstruction_annealing(
     qubos: Dict,
-    seed: int,
     num_anneal_fractions: int,
 ):
     fractions = np.linspace(0, 1, num=num_anneal_fractions, endpoint=True)
-    first = True
 
     results = {}
     for i, qubo in qubos.items():
+        if qubo.size == 0:
+            log.warning(f"Skipping qubo {i+1}/{len(qubos)}")
+            continue
         log.info(f"Computing spectral gaps for QUBO {i+1}/{len(qubos)}")
         results[i] = track_reconstruction_annealing(
             qubo,
-            seed,
             fractions,
-            geometric_index=i,
-            include_header=first,  # FIXME
         )
-        first = False
 
+    results = pd.DataFrame.from_dict(results)
     return {"results": results}
--- src/fromhopetoheuristics/pipelines/adiabatic_trackrec/pipeline.py
+++ src/fromhopetoheuristics/pipelines/adiabatic_trackrec/pipeline.py
@@ -12,11 +12,11 @@ def create_pipeline() -> Pipeline:
                 run_track_reconstruction_annealing,
                 {
                     "qubos": "qubos",
-                    "event_path": "event_path",
-                    "seed": "params:seed",
                     "num_anneal_fractions": "params:num_anneal_fractions",
                 },
-                {},
+                {
+                    "results": "adiabatic_track_reconstruction_results",
+                },
             ),
         ]
     )
