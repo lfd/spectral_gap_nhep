e91e264449bd66f14ca5e81f05dda9060eab6134
lc3267 <lc3267@kit.edu>
2024-10-22 09:08:18 +0000

    gc
    Signed-off-by: lc3267 <lc3267@kit.edu>

--- src/fromhopetoheuristics/datasets/qallse_split.py
+++ src/fromhopetoheuristics/datasets/qallse_split.py
@@ -4,14 +4,11 @@ import pickle
 import os
 import glob
 
-import fsspec
 import numpy as np
-from fromhopetoheuristics.utils.model import QallseSplit
 from fromhopetoheuristics.utils.qaoa_utils import (
     dict_QUBO_to_matrix,
 )
 from kedro.io import AbstractDataset
-from kedro.io.core import get_filepath_str, get_protocol_and_path, Version
 
 from qallse import dumper
 
@@ -20,49 +17,15 @@ import logging
 log = logging.getLogger(__file__)
 
 
-class QallseSplitDataset(AbstractDataset):
-    def __init__(self, filepath: str):
-        protocol, path = get_protocol_and_path(filepath)
-        self._protocol = protocol
-        self._fs = fsspec.filesystem(self._protocol)
-
-        super().__init__(
-            filepath=PurePosixPath(path),
-            exists_function=self._fs.exists,
-            glob_function=self._fs.glob,
-        )
-
-    # def load(self) -> np.ndarray:
-    #     load_path = get_filepath_str(self._get_load_path(), self._protocol)
-    #     with self._fs.open(load_path, mode="r") as f:
-    #         image = Image.open(f).convert("RGBA")
-    #         return np.asarray(image)
-
-    def save(self, model: QallseSplit, qubo_prefix: str) -> None:
-        qubo_path = os.path.join(self._get_save_path(), "QUBO")
-        os.makedirs(qubo_path, exist_ok=True)
-        dumper.dump_model(
-            model,
-            qubo_path,
-            qubo_prefix,
-            qubo_kwargs=dict(w_marker=None, c_marker=None),
-        )
-        qubo_path = os.path.join(qubo_path, qubo_prefix + "qubo.pickle")
-        log.info("Wrote qubo to", qubo_path)
-
-    def _describe(self) -> Dict[str, Any]:
-        return dict(
-            filepath=self._filepath, version=self._version, protocol=self._protocol
-        )
-
-
 class QuboDataset(AbstractDataset):
-    def __init__(self, filepath: str):
+
+    def __init__(self, filepath: str, **kwargs):
         self._filepath = PurePosixPath(filepath)
 
     def load(self) -> np.ndarray:
         qubos = {}
         qubos_paths = glob.glob(os.path.join(self._filepath, "*qubo.pickle"))
+        self._n_qubos = len(qubos_paths)
         for i, qubo_path in enumerate(qubos_paths):
             with open(qubo_path, "rb") as f:
                 Q = pickle.load(f)
@@ -90,6 +53,9 @@ class QuboDataset(AbstractDataset):
             )
 
     def _describe(self) -> Dict[str, Any]:
+        qubos_paths = glob.glob(os.path.join(self._filepath, "*qubo.pickle"))
+
         return dict(
             filepath=self._filepath,
+            number_of_qubos=len(qubos_paths),
         )
