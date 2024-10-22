b21f10bdd523164883d4599ad9e90a2f21326518
lc3267 <lc3267@kit.edu>
2024-10-22 08:44:30 +0000

    adjusted anneal schedules to support catalog
    Signed-off-by: lc3267 <lc3267@kit.edu>

--- src/fromhopetoheuristics/pipelines/anneal_schedule/nodes.py
+++ src/fromhopetoheuristics/pipelines/anneal_schedule/nodes.py
@@ -1,10 +1,5 @@
-import os
 import numpy as np
-
-from fromhopetoheuristics.utils.data_utils import (
-    load_params_from_csv,
-    save_to_csv,
-)
+import pandas as pd
 
 import logging
 
@@ -16,64 +11,83 @@ log = logging.getLogger(__name__)
 
 
 def create_maxcut_anneal_schedule(
-    q: int,
-    max_p: int,
+    results,
     maxcut_max_qubits: int = 10,
 ):
-    header_content = ["problem", "p", "q", "anneal_time", "anneal_fraction"]
+    # header_content = ["problem", "p", "q", "anneal_time", "anneal_fraction"]
 
-    header_content.extend(["n", "density"])
-    save_to_csv(header_content, result_path_prefix, "anneal_schedule.csv")
+    # header_content.extend(["n", "density"])
+    # save_to_csv(header_content, result_path_prefix, "anneal_schedule.csv")
+
+    result = {}
     for n in range(4, maxcut_max_qubits + 1):
+        result[n] = {}
         for density in np.linspace(0.5, 1, num=6, endpoint=True):
-            betas, gammas = load_params_from_csv(
-                qaoa_result_file,
-                q=q,
-                p=max_p,
-                num_qubits=n,
-                density=density,
-            )
+            # betas, gammas = load_params_from_csv(
+            #     qaoa_result_file,
+            #     q=q,
+            #     p=max_p,
+            #     num_qubits=n,
+            #     density=density,
+            # )
+            betas = results[f"{n}"][f"{density}"]["betas"]
+            gammas = results[f"{n}"][f"{density}"]["gammas"]
+
             if betas is None or gammas is None:
                 continue
 
             anneal_schedule = annealing_schedule_from_QAOA_params(betas, gammas)
 
-            for anneal_time, anneal_fraction in anneal_schedule:
-                row_content = [
-                    "maxcut",
-                    max_p,
-                    q,
-                    anneal_time,
-                    anneal_fraction,
-                    n,
-                    density,
-                ]
-                save_to_csv(row_content, result_path_prefix, "anneal_schedule.csv")
+            result[n][density] = anneal_schedule
+            # for anneal_time, anneal_fraction in anneal_schedule:
+            #     row_content = [
+            #         "maxcut",
+            #         max_p,
+            #         q,
+            #         anneal_time,
+            #         anneal_fraction,
+            #         n,
+            #         density,
+            #     ]
+            #     save_to_csv(row_content, result_path_prefix, "anneal_schedule.csv")
+
+    results = pd.DataFrame.from_dict(result)
+    return {"results": results}
 
 
 def create_trackrecon_anneal_schedule(
-    q: int,
-    max_p: int,
+    results,
     num_angle_parts: int = 64,
 ):
-    header_content.append("geometric_index")
-    save_to_csv(header_content, result_path_prefix, "anneal_schedule.csv")
+    # header_content.append("geometric_index")
+    # save_to_csv(header_content, result_path_prefix, "anneal_schedule.csv")
+
+    result = {}
     for i in range(num_angle_parts):
-        betas, gammas = load_params_from_csv(
-            qaoa_result_file, q=q, p=max_p, geometric_index=i
-        )
+        # betas, gammas = load_params_from_csv(
+        #     qaoa_result_file, q=q, p=max_p, geometric_index=i
+        # )
+        betas = results[f"{i}"]["betas"]
+        gammas = results[f"{i}"]["gammas"]
+
         if betas is None or gammas is None:
             continue
 
         anneal_schedule = annealing_schedule_from_QAOA_params(betas, gammas)
 
-        for anneal_time, anneal_fraction in anneal_schedule:
-            row_content = [
-                "track reconstruction",
-                max_p,
-                q,
-                anneal_time,
-                anneal_fraction,
-                i,
-            ]
-            save_to_csv(row_content, result_path_prefix, "anneal_schedule.csv")
+        results[i] = anneal_schedule
+
+        # for anneal_time, anneal_fraction in anneal_schedule:
+
+        #     row_content = [
+        #         "track reconstruction",
+        #         max_p,
+        #         q,
+        #         anneal_time,
+        #         anneal_fraction,
+        #         i,
+        #     ]
+        #     save_to_csv(row_content, result_path_prefix, "anneal_schedule.csv")
+
+    results = pd.DataFrame.from_dict(results)
+    return {"results": results}
--- src/fromhopetoheuristics/pipelines/anneal_schedule/pipeline.py
+++ src/fromhopetoheuristics/pipelines/anneal_schedule/pipeline.py
@@ -3,26 +3,31 @@ from kedro.pipeline import Pipeline, node, pipeline
 from .nodes import create_maxcut_anneal_schedule, create_trackrecon_anneal_schedule
 
 
-def create_pipeline() -> Pipeline:
+def create_maxcut_pipeline() -> Pipeline:
     return pipeline(
         [
             node(
                 create_maxcut_anneal_schedule,
                 {
-                    "q": "params:q",
-                    "max_p": "params:max_p",
+                    "results": "qaoa_maxcut_results",
                     "maxcut_max_qubits": "params:maxcut_max_qubits",
                 },
-                {},
+                {"results": "anneal_schedule_maxcut_results"},
             ),
+        ]
+    )
+
+
+def create_trackrecon_pipeline() -> Pipeline:
+    return pipeline(
+        [
             node(
                 create_trackrecon_anneal_schedule,
                 {
-                    "q": "params:q",
-                    "max_p": "params:max_p",
+                    "results": "qaoa_track_reconstruction_results",
                     "num_angle_parts": "params:num_angle_parts",
                 },
-                {},
+                {"results": "anneal_schedule_track_reconstruction_results"},
             ),
         ]
     )
