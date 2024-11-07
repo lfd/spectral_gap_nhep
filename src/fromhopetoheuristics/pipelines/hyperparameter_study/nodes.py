932d9f7a8f7c77af300ca5ddf64b6b21ec7ddd4d
lc3267 <lc3267@kit.edu>
2024-11-07 20:10:08 +0000

    added drafty hyperparam stuff
    Signed-off-by: lc3267 <lc3267@kit.edu>

---
+++ src/fromhopetoheuristics/pipelines/hyperparameter_study/nodes.py
@@ -0,0 +1,79 @@
+from typing import Dict, List
+
+from fromhopetoheuristics.utils.hyperparam_optimizer import Hyperparam_Optimizer
+
+import logging
+
+log = logging.getLogger(__name__)
+
+
+def create_hyperparam_optimizer(
+    n_trials: str,
+    timeout: int,
+    enabled_hyperparameters: List,
+    optimization_metric: List,
+    path: str,
+    sampler: str,
+    sampler_seed: int,
+    pool_process: bool,
+    pruner_strategy: str,
+    pruner_startup_trials: int,
+    pruner_warmup_steps: int,
+    pruner_interval_steps: int,
+    pruner_min_trials: int,
+    selective_optimization: bool,
+    resume_study: bool,
+    n_jobs: int,
+    run_id: str,
+    hyperparameters: Dict,
+) -> Hyperparam_Optimizer:
+    if run_id is None:
+        name = mlflow.active_run().info.run_id
+    else:
+        name = run_id
+
+    hyperparam_optimizer = Hyperparam_Optimizer(
+        name=name,
+        sampler=sampler,
+        seed=sampler_seed,
+        n_trials=n_trials,
+        timeout=timeout,
+        enabled_hyperparameters=enabled_hyperparameters,
+        optimization_metric=optimization_metric,
+        path=path,
+        n_jobs=n_jobs,
+        selective_optimization=selective_optimization,
+        resume_study=resume_study,
+        pool_process=pool_process,
+        pruner=pruner_strategy,
+        pruner_startup_trials=pruner_startup_trials,
+        pruner_warmup_steps=pruner_warmup_steps,
+        pruner_interval_steps=pruner_interval_steps,
+        pruner_min_trials=pruner_min_trials,
+    )
+
+    hyperparam_optimizer.set_variable_parameters(hyperparameters)
+    hyperparam_optimizer.set_fixed_parameters({})
+
+    def objective(trial, parameters, report_callback, early_stop_callback):
+        raise NotImplementedError("Objective method must be set!")
+
+    hyperparam_optimizer.objective = objective
+
+    return {"hyperparam_optimizer": hyperparam_optimizer}
+
+
+def run_optuna(
+    hyperparam_optimizer: Hyperparam_Optimizer,
+):
+    hyperparam_optimizer.minimize(idx=0)
+
+    # try:
+    #     hyperparam_optimizer.log_study(
+    #         selected_parallel_params=optuna_selected_parallel_params,
+    #         selected_slice_params=optuna_selected_slice_params,
+    #     )
+    # except Exception as e:
+    #     log.exception("Error while logging study")
+
+    return {}
---
+++ src/fromhopetoheuristics/pipelines/hyperparameter_study/pipeline.py
@@ -0,0 +1,44 @@
+from kedro.pipeline import Pipeline, node, pipeline
+
+from .nodes import create_hyperparam_optimizer, run_optuna
+
+
+def create_pipeline() -> Pipeline:
+
+    return pipeline(
+        [
+            node(
+                create_hyperparam_optimizer,
+                inputs={
+                    "n_trials": "params:optuna.n_trials",
+                    "timeout": "params:optuna.timeout",
+                    "enabled_hyperparameters": "params:optuna.enabled_hyperparameters",
+                    "optimization_metric": "params:optuna.optimization_metric",
+                    "path": "params:optuna.path",
+                    "sampler": "params:optuna.sampler",
+                    "sampler_seed": "params:optuna.sampler_seed",
+                    "pool_process": "params:optuna.pool_process",
+                    "pruner_strategy": "params:optuna.pruner_strategy",
+                    "pruner_startup_trials": "params:optuna.pruner_startup_trials",
+                    "pruner_warmup_steps": "params:optuna.pruner_warmup_steps",
+                    "pruner_interval_steps": "params:optuna.pruner_interval_steps",
+                    "pruner_min_trials": "params:optuna.pruner_min_trials",
+                    "selective_optimization": "params:optuna.selective_optimization",
+                    "resume_study": "params:optuna.resume_study",
+                    "n_jobs": "params:optuna.n_jobs",
+                    "run_id": "params:optuna.run_id",
+                    "hyperparameters": "params:optuna.hyperparameters",
+                },
+                outputs={
+                    "hyperparam_optimizer": "hyperparam_optimizer",
+                },
+            ),
+            node(
+                run_optuna,
+                inputs={
+                    "hyperparam_optimizer": "hyperparam_optimizer",
+                },
+                outputs={},
+            ),
+        ]
+    )
